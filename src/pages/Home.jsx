6e9edb0497fdff8ec9d8fec53d39fa74b71974bb
SHIVAM KUMAR <shivamjha2705@gmail.com>
2025-01-05 14:17:02 +0530

    initial setup

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import Footer from "../components/footer/Footer"
+import Header from "../components/header/Header"
+
+const Home = () => {
+  return (
+    <>
+    <Header/>
+<div>Home</div>
+<Footer/>
+    </>
+    
+  )
+}
+
+export default Home
