8dadbda80f99c396016e0c0d25ce5fc0026d30d4
SHIVAM KUMAR <shivamjha2705@gmail.com>
2025-01-05 14:21:33 +0530

    formated the home page structure

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,14 +2,14 @@ import Footer from "../components/footer/Footer"
 import Header from "../components/header/Header"
 
 const Home = () => {
-  return (
-    <>
-    <Header/>
-<div>Home</div>
-<Footer/>
-    </>
-    
-  )
+    return (
+        <>
+            <Header />
+            <div>Home</div>
+            <Footer />
+        </>
+
+    )
 }
 
 export default Home
