d4687104c96c2bfc5e191b585bc349db162b46b5
SHIVAM KUMAR <shivamjha2705@gmail.com>
2025-05-22 21:56:54 +0530

    Added assets

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,3 +1,18 @@
 export { default as Logo } from './store_logo.png';
 export { default as slider1 } from './slider-1.png';
 export { default as slider2 } from './slider-2.png';
+
+export { default as vegetable } from './vegetable.png';
+export { default as cakeMilk } from './cakeMilk.png';
+export { default as coffee } from './coffee.png';
+export { default as custardApple } from './custardApple.png';
+export { default as snack } from './snack.png';
+export { default as kiwi } from './kiwi.png';
+export { default as strawberry } from './strawberry.png';
+export { default as blackplum } from './blackplum.png';
+export { default as apple } from './apple.png';
+export { default as peach } from './peach.png';
+
+export { default as banner1 } from './banner-1.png';
+export { default as banner2 } from './banner-2.png';
+export { default as banner3 } from './banner-3.png';
