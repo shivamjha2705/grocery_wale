667f8d31a97531f1860ed16edba8af64f468480b
SHIVAM KUMAR <shivamjha2705@gmail.com>
2025-05-22 22:01:30 +0530

    added constant data to separate contants folder

---
+++ src/constants/categories.js
@@ -0,0 +1,12 @@
+export const categories = [
+    "Milk and Dairies",
+    "Clothing & Beauty",
+    "Fresh Seafood",
+    "Wines & Drinks",
+    "Pet Foods & Toy",
+    "Fast Food",
+    "Baking Material",
+    "Vegetables",
+    "Fresh Fruit",
+    "Bread and Juice",
+  ];
